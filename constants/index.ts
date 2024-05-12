116da1f0bb870463d4a5091b671e6f8b6c2a29c4
NuttaphonT <158470347+NuttaphonT@users.noreply.github.com>
2024-05-12 22:22:42 +0700

    first commit

---
+++ constants/index.ts
@@ -0,0 +1,6 @@
+export const ROP2E_ITEM_IMG_URL = "https://cdn.maxion.gg/landverse/image/item";
+export const ROP2E_COLLECTION_IMG_URL =
+  "https://cdn.maxion.gg/landverse/image/collection";
+export const ROP2E_MAP_IMG_URL = "https://cdn.maxion.gg/landverse/image/map";
+export const ROP2E_MONSTER_IMG_URL =
+  "https://cdn.maxion.gg/landverse/image/monster";
